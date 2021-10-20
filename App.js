cff4cdde8baa213a2c5fde17727db8c6c1218d19
Shevon Soyza <shevonrevinston@gmail.com>
2021-10-20 10:31:22 +0530

    Project Setup

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Project set up</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ ios/skyline/ForceBridgingHeader.swift
@@ -0,0 +1,8 @@
+//
+//  ForceBridgingHeader.swift
+//  skyline
+//
+//  Created by shevon soyza on 2021-10-20.
+//
+
+import Foundation
